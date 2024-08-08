6eebe2d5bfa03c58701ec43f272889cedc139cbe
Tony <fedosov-toxa@mail.ru>
2024-08-08 19:17:01 +0300

    add test site

---
+++ innouttest/innouttest/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#+3@-_(nzw6lv^_k$=9@^q)0&4ct!q%j*mhrgxq49#i_gwt3q*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

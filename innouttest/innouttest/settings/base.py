e9d0fb7f1f1fb58c557196fd490decaab57fa62f
Tony <fedosov-toxa@mail.ru>
2024-08-17 11:22:11 +0300

    init commit

--- innouttest/innouttest/settings/base.py
+++ innouttest/innouttest/settings/base.py
@@ -85,9 +85,16 @@ WSGI_APPLICATION = "innouttest.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'innout_test',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        # 'OPTIONS': {
+        #   'sslmode': 'require'
+        # }
     }
 }
 

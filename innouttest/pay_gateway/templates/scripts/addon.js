300454eac8ecccf6d006c03575936663c48c2dba
Tony <fedosov-toxa@mail.ru>
2024-08-23 01:16:59 +0300

    need to change combobox value

--- innouttest/pay_gateway/templates/scripts/addon.js
+++ innouttest/pay_gateway/templates/scripts/addon.js
@@ -5,7 +5,7 @@ function getCheckedCheckBoxes() {
     let s11;
     let s14;
     let peopleAmount = document.getElementById("value_range");
-    s11 = x12.innerHTML.toString();
+    s11 = x12.innerHTML;
     peopleAmount = Number(peopleAmount.innerHTML.toString());
     if (s11 == "INOUT Проект База") {
         resultSum += 590 * peopleAmount;
@@ -72,98 +72,104 @@ function getCheckedCheckBoxes() {
 }
 
 
-function customSelect() {
-    var x, i, j, l, ll, selElmnt, a, b, c;
-    /* ищем любой элемент с классом "custom-select" */
-    x = document.getElementsByClassName("custom-select");
-    const x1 = document.getElementsByClassName("hidden_block");
-    l = x.length;
-    for (i = 0; i < l; i++) {
-        selElmnt = x[i].getElementsByTagName("select")[0];
-        ll = selElmnt.length;
-        /* для каждого элемента создаем новый элемент DIV, который будет работать как элемент выбора */
-        a = document.createElement("DIV");
-        a.setAttribute("class", "select-selected");
-        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-        x[i].appendChild(a);
-        /* для каждого элемента создаем новый элемент DIV, который будет содержать список опций */
-        b = document.createElement("DIV");
-        b.setAttribute("class", "select-items select-hide");
-        for (j = 0; j < ll; j++) {
-            /* для каждой опции в оригинальном выпадающем списке
-            создаем новый элемент DIV, который будет работать как опция */
-            c = document.createElement("DIV");
-            c.innerHTML = selElmnt.options[j].innerHTML;
-            c.addEventListener("click", function (e) {
-                /* когда на элемент кликают, обновляем оригинальный выпадающий список
-                и выбранный элемент */
-                var y, i, k, s, h, sl, yl;
-                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-                sl = s.length;
-                h = this.parentNode.previousSibling;
-
-                let blocks = document.querySelectorAll('.show');
-                // скрываем их
-                blocks.forEach(block => {
-                    block.style.display = "none";
-                });
-                document.getElementById("help_desk").checked = false;
-                document.getElementById("basic_dms").checked = false;
-                document.getElementById("risk_management").checked = false;
-                document.getElementById("custom_branding").checked = false;
-                document.getElementById("gitlab_integration").checked = false;
-                document.getElementById("b2b_crm").checked = false;
-                document.getElementById("knowledge_base").checked = false;
-                document.getElementById("asset_config").checked = false;
-                document.getElementById("private_cloud").checked = false;
-                for (i = 0; i < sl; i++) {
-                    if (s.options[i].innerHTML == this.innerHTML) {
-                        s.selectedIndex = i;
-                        h.innerHTML = this.innerHTML;
-                        y = this.parentNode.getElementsByClassName("same-as-selected");
-                        yl = y.length;
-                        x1.innerHTML = this.innerHTML;
-                        if (i == 0) {
-                            blocks = document.querySelectorAll('.show_0');
-                            blocks.forEach(block => {
-                                block.style.display = "block";
-                            });
-                        } else if (i == 1) {
-                            blocks = document.querySelectorAll('.show_1');
-                            blocks.forEach(block => {
-                                block.style.display = "block";
-                            });
-                        } else if (i == 2) {
-                            blocks = document.querySelectorAll('.show_2');
-                            blocks.forEach(block => {
-                                block.style.display = "block";
-                            });
-                        }
-                        getCheckedCheckBoxes()
-                        for (k = 0; k < yl; k++) {
-                            y[k].removeAttribute("class");
-                        }
-                        this.setAttribute("class", "same-as-selected");
-                        break;
-                    }
-                }
-                h.click();
-            });
-            b.appendChild(c);
-        }
-        x[i].appendChild(b);
-        a.addEventListener("click", function (e) {
-            /* когда на выпадающий список кликают, закрываем другие элементы выбора
-            и открываем/закрываем текущий элемент выбора */
-            e.stopPropagation();
-            closeAllSelect(this);
-            this.nextSibling.classList.toggle("select-hide");
-            this.classList.toggle("select-arrow-active");
-        });
-    }
+function test_onload() {
+    let plan_text = localStorage.getItem('test');
+    let x12 = document.getElementsByClassName("hidden_block");
+    x12.innerHTML = plan_text;
+    getCheckedCheckBoxes();
 }
 
+test_onload();
 
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* ищем любой элемент с классом "custom-select" */
+x = document.getElementsByClassName("custom-select");
+const x1 = document.getElementsByClassName("hidden_block");
+l = x.length;
+for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /* для каждого элемента создаем новый элемент DIV, который будет работать как элемент выбора */
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /* для каждого элемента создаем новый элемент DIV, который будет содержать список опций */
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 0; j < ll; j++) {
+        /* для каждой опции в оригинальном выпадающем списке
+        создаем новый элемент DIV, который будет работать как опция */
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            /* когда на элемент кликают, обновляем оригинальный выпадающий список
+            и выбранный элемент */
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+
+            let blocks = document.querySelectorAll('.show');
+            // скрываем их
+            blocks.forEach(block => {
+                block.style.display = "none";
+            });
+            document.getElementById("help_desk").checked = false;
+            document.getElementById("basic_dms").checked = false;
+            document.getElementById("risk_management").checked = false;
+            document.getElementById("custom_branding").checked = false;
+            document.getElementById("gitlab_integration").checked = false;
+            document.getElementById("b2b_crm").checked = false;
+            document.getElementById("knowledge_base").checked = false;
+            document.getElementById("asset_config").checked = false;
+            document.getElementById("private_cloud").checked = false;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    yl = y.length;
+                    x1.innerHTML = this.innerHTML;
+                    if (i == 0) {
+                        blocks = document.querySelectorAll('.show_0');
+                        blocks.forEach(block => {
+                            block.style.display = "block";
+                        });
+                    } else if (i == 1) {
+                        blocks = document.querySelectorAll('.show_1');
+                        blocks.forEach(block => {
+                            block.style.display = "block";
+                        });
+                    } else if (i == 2) {
+                        blocks = document.querySelectorAll('.show_2');
+                        blocks.forEach(block => {
+                            block.style.display = "block";
+                        });
+                    }
+                    getCheckedCheckBoxes()
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        /* когда на выпадающий список кликают, закрываем другие элементы выбора
+        и открываем/закрываем текущий элемент выбора */
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
 
 
 function closeAllSelect(elmnt) {
@@ -195,95 +201,94 @@ function closeAllSelectCustomAddons() {
 }
 
 
-function customRange() {
-    let myRange = document.querySelector('.range-input');
-    let myValue = document.querySelector('#myValue');
-    let off = (myRange.offsetWidth - 30) / (parseInt(myRange.max) - parseInt(myRange.min));
-    let px = ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.offsetParent.offsetWidth / 2);
+let myRange = document.querySelector('.range-input');
+let myValue = document.querySelector('#myValue');
+let off = (myRange.offsetWidth - 30) / (parseInt(myRange.max) - parseInt(myRange.min));
+let px = ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.offsetParent.offsetWidth / 2);
 
-    myValue.parentElement.style.left = px + 11 + 'px';
-    myValue.parentElement.style.top = myRange.offsetHeight + 'px';
+myValue.parentElement.style.left = px + 11 + 'px';
+myValue.parentElement.style.top = myRange.offsetHeight + 'px';
+myValue.innerHTML = myRange.value;
+
+myRange.oninput = function () {
+    let px = ((myRange.valueAsNumber + 2 - parseInt(myRange.min)) * off) - ((myValue.offsetWidth - 5) / 2);
     myValue.innerHTML = myRange.value;
+    myValue.parentElement.style.left = px + 'px';
+    getCheckedCheckBoxes()
+    // document.querySelector("#price_panel_about_label_id").innerHTML = 'Итоговая цена за ' + myRange.valueAsNumber.toString() + ' пользователей';
 
-    myRange.oninput = function () {
-        let px = ((myRange.valueAsNumber + 2 - parseInt(myRange.min)) * off) - ((myValue.offsetWidth - 5) / 2);
-        myValue.innerHTML = myRange.value;
-        myValue.parentElement.style.left = px + 'px';
-        getCheckedCheckBoxes()
-        // document.querySelector("#price_panel_about_label_id").innerHTML = 'Итоговая цена за ' + myRange.valueAsNumber.toString() + ' пользователей';
+};
 
-    };
+var sliderEl4 = document.querySelector("#range4")
+var sliderValue4 = document.querySelector(".value4")
 
-    var sliderEl4 = document.querySelector("#range4")
-    var sliderValue4 = document.querySelector(".value4")
+sliderEl4.addEventListener("input", (event) => {
+    var tempSliderValue = event.target.value;
+    sliderValue4.textContent = tempSliderValue;
 
-    sliderEl4.addEventListener("input", (event) => {
-        var tempSliderValue = event.target.value;
-        sliderValue4.textContent = tempSliderValue;
+    var progress = (tempSliderValue / sliderEl4.max) * 100;
 
-        var progress = (tempSliderValue / sliderEl4.max) * 100;
-
-        sliderEl4.style.background = `linear-gradient(to right, #2066F1 ${progress}%, #ccc ${progress}%)`;
-        document.querySelector('#price_panel_about_label_id').innerHTML = 'Итоговая цена за ' + progress.toString() + ' пользователей';
-        // document.querySelector('#range_label').innerHTML = progress.toString();
+    sliderEl4.style.background = `linear-gradient(to right, #2066F1 ${progress}%, #ccc ${progress}%)`;
+    document.querySelector('#price_panel_about_label_id').innerHTML = 'Итоговая цена за ' + progress.toString() + ' пользователей';
+    // document.querySelector('#range_label').innerHTML = progress.toString();
 
 
-    })
+})
 
-    let resultSum, checkboxHelpDesk, checkboxDms,
-        checkboxRisk, checkboxBrand, checkboxGit,
-        checkboxB2b, checkboxBase, checkboxManage,
-        checkboxCloud, redioYear, radioMonth;
-    resultSum = 0;
-    checkboxHelpDesk = document.getElementById("help_desk")
-    checkboxDms = document.getElementById("basic_dms")
-    checkboxRisk = document.getElementById("risk_management")
-    checkboxBrand = document.getElementById("custom_branding")
-    checkboxGit = document.getElementById("gitlab_integration")
-    checkboxB2b = document.getElementById("b2b_crm")
-    checkboxBase = document.getElementById("knowledge_base")
-    checkboxManage = document.getElementById("asset_config")
-    checkboxCloud = document.getElementById("private_cloud")
-    redioYear = document.getElementById("in_year")
-    radioMonth = document.getElementById("in_month")
+let resultSum, checkboxHelpDesk, checkboxDms,
+    checkboxRisk, checkboxBrand, checkboxGit,
+    checkboxB2b, checkboxBase, checkboxManage,
+    checkboxCloud, redioYear, radioMonth;
+resultSum = 0;
+checkboxHelpDesk = document.getElementById("help_desk")
+checkboxDms = document.getElementById("basic_dms")
+checkboxRisk = document.getElementById("risk_management")
+checkboxBrand = document.getElementById("custom_branding")
+checkboxGit = document.getElementById("gitlab_integration")
+checkboxB2b = document.getElementById("b2b_crm")
+checkboxBase = document.getElementById("knowledge_base")
+checkboxManage = document.getElementById("asset_config")
+checkboxCloud = document.getElementById("private_cloud")
+redioYear = document.getElementById("in_year")
+radioMonth = document.getElementById("in_month")
 
-    checkboxHelpDesk.addEventListener('change', (event) => {
-        getCheckedCheckBoxes();
-    })
-    checkboxDms.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxRisk.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxBrand.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxGit.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxB2b.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxBase.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxManage.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    checkboxCloud.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    redioYear.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-    radioMonth.addEventListener('change', (event) => {
-        getCheckedCheckBoxes()
-    })
-}
+checkboxHelpDesk.addEventListener('change', (event) => {
+    getCheckedCheckBoxes();
+})
+checkboxDms.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxRisk.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxBrand.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxGit.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxB2b.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxBase.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxManage.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+checkboxCloud.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+redioYear.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
+radioMonth.addEventListener('change', (event) => {
+    getCheckedCheckBoxes()
+})
 
-const hidden_plan = document.querySelectorAll('.hidden_plan');
-if (hidden_plan.length) {
-   customSelect();
-   customRange();
-}
+
+
+
+
+
+
---
+++ innouttest/pay_gateway/templates/scripts/buttons_page.js
@@ -0,0 +1,11 @@
+function setPlanOne() {
+    localStorage.setItem('test', "INOUT Проект База");
+}
+
+function setPlanTwo() {
+    localStorage.setItem('test', "INOUT Проект Бизнес");
+}
+
+function setPlanThree() {
+    localStorage.setItem('test', "INOUT Проект Платформа");
+}

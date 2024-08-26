17d3f6afe7237a9d64755e0fc81936290c5085a8
Tony <fedosov-toxa@mail.ru>
2024-08-26 19:33:32 +0300

    add to info_about_user.js points to numbers

--- innouttest/pay_gateway/templates/scripts/info_about_user.js
+++ innouttest/pay_gateway/templates/scripts/info_about_user.js
@@ -1,7 +1,9 @@
 function on_load() {
     document.querySelector('.plan_and_users').innerHTML = localStorage.getItem('plan') + " - " + localStorage.getItem('userAmount') + " пользователей";
-    document.querySelector('.price_per_month').innerHTML = "(" + localStorage.getItem('resultSumMonth') + " руб / месяц)";
-    document.querySelector('.price_per_year').innerHTML = localStorage.getItem('resultSumYear') + " руб / год";
-    document.querySelector('#price_per_month_2').innerHTML = localStorage.getItem('resultSumMonth') + " руб / год";
+    let resultSumYear = (localStorage.getItem('resultSumYear') + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1.');
+    let resultSumMonth = (localStorage.getItem('resultSumMonth') + '').replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1.');
+    document.querySelector('.price_per_month').innerHTML = "(" + resultSumMonth + " руб / месяц)";
+    document.querySelector('.price_per_year').innerHTML = resultSumYear + " руб / год";
+    document.querySelector('#price_per_month_2').innerHTML = resultSumMonth + " руб / месяц";
 }
 on_load();

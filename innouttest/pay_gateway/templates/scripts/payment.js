e9bb40a70e52b626fd0cbf1e376f8058c4678311
Tony <fedosov-toxa@mail.ru>
2024-09-01 00:03:52 +0300

    hidden script in progress

--- innouttest/pay_gateway/templates/scripts/payment.js
+++ innouttest/pay_gateway/templates/scripts/payment.js
@@ -1,8 +1,16 @@
 function hidden_about() {
+    document.write('HEllo')
+    let element = document.getElementById("hidden_text_about_payment_id");
+    // if (element.isv == 'hidden'){
+    //     element.style.visibility = 'visible';
+    // }
+    // else
     element.style.visibility = 'hidden';      // Hide
-    element.style.visibility = 'visible';
 
 }
+
+let hidden_element = document.getElementById("text_about_payment_id");
+hidden_element.onclick(hidden_about);
 
 function setPrice() {
     document.querySelector('.text_result_price').innerHTML = localStorage.getItem('resSumPay') + '  руб';

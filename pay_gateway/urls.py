ffa97a4c16cb8028a16fa0bdee122814ccc5c1dc
Tony <fedosov-toxa@mail.ru>
2024-10-14 13:35:17 +0300

    add ons change test v3

--- pay_gateway/urls.py
+++ pay_gateway/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('choose_plan/', views.plan_view, name='plan_view'),
     path('add_ons/', views.add_ons, name='add_ons'),
-    path('add_users/', views.add_users, name='add_users'),
+    path('payment/add_users/', views.add_users, name='add_users'),
     path('payment/', views.payment, name='payment'),
     path('accept_payment/', views.accept_payment, name='accept_payment'),
     path('unsuccess_payment/', views.unsuccess_payment, name='unsuccess_payment'),

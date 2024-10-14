c5eb468247f0ad86c0bbcd734d5b639f3f8cfa23
Tony <fedosov-toxa@mail.ru>
2024-10-14 17:14:20 +0300

    test migrations

--- pay_gateway/models.py
+++ pay_gateway/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+class ForDelete(models.Model):
+    name = models.CharField(max_length=120)
+
 # Модель для плана
 class Plan(models.Model):
     name = models.CharField(max_length=100)  # Имя плана
